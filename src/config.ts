d9d001a5f68b1d4bbe461859b5e257b6fc1ba7d8
Jack Ault <henlowgg@outlook.com>
2024-01-06 09:13:32 -0500

    theme change, going with darker

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 // global data
 
 export const SITE_TITLE = 'Link Lee Portfolio';
-export const SITE_DESCRIPTION = 'Fun little portfolio update to bring in the new year!';
+export const SITE_DESCRIPTION = 'Link Lee Portfolio';
